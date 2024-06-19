01d28de89370ec7aa80db964c4e4e8f01d1d2372
DanieleBernassola <danielebernassola02@gmail.com>
2024-06-19 15:35:05 +0200

    [UPD] Created form and function

--- index.php
+++ index.php
@@ -1,4 +1,25 @@
 <?php
+
+// LENGTH INSERITA IN INPUT
+$length = $_GET['length'];
+
+// FUNZIONE GENERA PASSWORD
+function generatePassword($length)
+{
+  $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-=[]{}|;:,.<>?';
+  $password = '';
+  // CHARACTERSLENGTH VERRà UTILIZZATO COME VALORE MASSIMO PER PRENDERE UN CARATTERE CASUALE
+  $charactersLength = strlen($characters);
+  for ($i = 0; $i < $length; $i++) {
+    $password .= $characters[rand(0, $charactersLength - 1)];
+  }
+  return $password;
+};
+
+if ($length > 0) {
+  $password = generatePassword($length);
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -11,7 +32,14 @@
 </head>
 
 <body>
-
+  <!-- FORM -->
+  <form action="index.php" method="get">
+    <label for="length">Scegli la lunghezza della password</label>
+    <input type="number" placeholder="lunghezza" id="length">
+    <button>Invia</button>
+  </form>
+  <h2>Password generata: </h2>
+  <p><?php echo $password ?></p>
 </body>
 
 </html>
